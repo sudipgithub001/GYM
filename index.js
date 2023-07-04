1564f1aeb4b4b283f09baeebd7e1eea6ff92aaf6
sudipgithub001 <sudip.chanda002@gmail.com>
2023-07-04 19:20:54 +0530

    yagnaya

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const User = require("./Models/userModel")
 // const { default: AboutUser } = require("../FRONTEND/src/components/aboutUser");
 app.use(cookieParser());
 // app.use(cors());
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+app.use(cors({ credentials: true, origin: 'https://excellencefitness2.netlify.app' }));
 
 dotenv.config();
 app.use(express.json());
